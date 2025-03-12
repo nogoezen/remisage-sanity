e37fbc34fe623a9f3b42a6b6a9c129fd8d3ba4ce
Haritiana Randria <93715284+nogoezen@users.noreply.github.com>
2025-03-12 10:02:11 -0400

    clean the project

--- server/src/controllers/index.ts
+++
@@ -1,6 +0,0 @@
-// Contrôleurs Sanity
-export { UserSanityController as UserController } from './UserSanityController';
-export { VehicleSanityController as VehicleController } from './VehicleSanityController';
-export { MessageSanityController as MessageController } from './MessageSanityController';
-export { RequestSanityController as RequestController } from './RequestSanityController';
-export { NotificationSanityController as NotificationController } from './NotificationSanityController'; 
